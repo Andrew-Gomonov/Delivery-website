2802421e08ca5e0ed8611bf369966d96cfa453bd
Array <nextmemd@gmail.com>
2023-01-12 22:33:12 +0200

    changed argParser.py

--- argParser.py
+++ argParser.py
@@ -1,23 +1,23 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser(
-    prog='DeliverySystem',
+    prog='DeliveryWebsite',
     description='Simplifies the delivery of goods',
-    epilog='With this system, delivery is easy',
+    epilog='With this website, delivery is easy',
     add_help=False
 )
 
 parser.add_argument(
     '--host',
     type=str,
-    help='host for this delivery system',
+    help='host for this delivery website',
     default="127.0.0.1",
     required=False
 )
 parser.add_argument(
     '--port',
     type=int,
-    help='the port on which the delivery system will operate',
+    help='the port on which the delivery website will operate',
     default=5000,
     required=False
 )
